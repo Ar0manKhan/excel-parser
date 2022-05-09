6ef567a8d72b4bdba256f9efec4fa3675b7442d0
Ar0manKhan <73093255+Ar0manKhan@users.noreply.github.com>
2022-05-09 21:35:52 +0530

    database setup

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 const express = require('express');
-// const myApp = require('./myApp');
+const mongoose = require('mongoose');
 const app = express();
+require('dotenv').config();
+
+// Connecting to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+// Creating schema for the database
+const { Schema } = mongoose;
+const personSchema = new Schema({
+	name: { type: String },
+	email: { type: String, required: true },
+	mobile: { type: String },
+	dob: { type: Date },
+	experience: { type: String },
+	title: { type: String },
+	location: { type: String },
+	postalAddress: { type: String },
+	currentEmployee: { type: String },
+	currentDesignation: { type: String }
+});
+
+
 
 app.use(express.static(__dirname + '/public'));
 
