6cec52bee531f2b9be0ec83f4237e17cbec81453
Ar0manKhan <73093255+Ar0manKhan@users.noreply.github.com>
2022-05-10 23:04:17 +0530

    backend setup complete

--- server.js
+++ server.js
@@ -1,35 +1,30 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
+const cors = require('cors');
+const multer = require('multer');
 require('dotenv').config();
+const { getSheetData, uploadData } = require('./utils');
 
-// Connecting to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
-});
-
-// Creating schema for the database
-const { Schema } = mongoose;
-const personSchema = new Schema({
-	name: { type: String },
-	email: { type: String, required: true },
-	mobile: { type: String },
-	dob: { type: Date },
-	experience: { type: String },
-	title: { type: String },
-	location: { type: String },
-	postalAddress: { type: String },
-	currentEmployee: { type: String },
-	currentDesignation: { type: String }
-});
-
-
-
+const app = express();
 app.use(express.static(__dirname + '/public'));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+// Setting up multer
+const upload = multer({ dest: './public/sheets/' });
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/views/index.html');
+})
+
+app.post('/upload', upload.single('file'), async (req, res) => {
+	try {
+		const file = req.file;	// getting excel sheet to process data
+		// getting all data from excel sheet
+		const fileData = await getSheetData(file.path);
+		await uploadData(fileData);
+		res.status(200);
+	} catch (e) {
+		res.status(500);
+	}
 })
 
 const port = process.env.PORT || 3000;
---
+++ utils.js
@@ -0,0 +1,99 @@
+const excel = require('read-excel-file/node')
+const mongoose = require('mongoose');
+const fs = require('fs');
+const async = require('async');
+const { create } = require('domain');
+const { createCipheriv } = require('crypto');
+
+// Connecting to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+// Creating schema for the database
+const { Schema } = mongoose;
+const personSchema = new Schema({
+	name: String,
+	email: { type: String, required: true },
+	mobile: String,
+	dob: Date,
+	experience: String,
+	title: String,
+	location: String,
+	postalAddress: String,
+	currentEmployee: String,
+	currentDesignation: String
+});
+const Person = mongoose.model('Person', personSchema);
+
+const excelJsonMap = {
+	"Name of the Candidate": 'name',
+	"Email": 'email',
+	"Mobile No.": 'mobile',
+	"Date of Birth": 'dob',
+	"Work Experience": 'experience',
+	"Resume Title": 'title',
+	"Current Location": 'location',
+	"Postal Address": 'postalAddress',
+	"Current Employer": 'currentEmployer',
+	"Current Designation": 'currentDesignation'
+}
+
+const removeFile = filepath => {
+	fs.unlink(filepath, err => {
+		if (err)
+			console.error(err);
+	})
+}
+
+/**
+ * Convert excel file to array of json
+ * @param {String} file Path of the excel sheet
+ * @returns json format of input excel sheet
+ */
+const getSheetData = async (file) => {
+	const excelData = await excel(fs.createReadStream(file), { map: excelJsonMap });
+	removeFile(file);
+	return excelData.rows;
+}
+
+/**
+ * It uploads person to the database
+ * @param {Person} data Data of person to upload
+ */
+const createPerson = async (data) => {
+	const newPerson = new Person(data);
+	try {
+		await newPerson.save(data);
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+/**
+ * Add person to database if email is unique otherwise just leave it
+ * @param {Person} data The data which is to be uploaded
+ */
+const uploadPersonData = async (data) => {
+	try {
+		const query = await Person.findOne({ email: data.email }).exec();
+		if (query === null)
+			await createPerson(data);
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+/**
+ * Adds all the person to database
+ * @param {*} data List of persons to add in the database
+ */
+const uploadData = async (data) => {
+	await async.eachSeries(data, async (item, callback) => {
+		await uploadPersonData(item);
+	})
+}
+
+exports.getSheetData = getSheetData;
+exports.uploadData = uploadData;
